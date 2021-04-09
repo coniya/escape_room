206f731780b590b66a9f635e9e86839c9067e7b0
coniya <82240913+coniya@users.noreply.github.com>
2021-04-09 17:05:26 -0500

    created the skeleton for the game
    what will be in each room, room class, game class, and main code

--- Text Based Escape Room.py
+++ Text Based Escape Room.py
@@ -1,1 +1,106 @@
 from tkinter import *
+import time
+
+#Room class
+class Room:
+    #constructor
+    def __init__(self, name, image):
+        #rooms have:
+        #a name ("room1", ...)
+        #an image
+        #items and item descriptions
+        #grabbables
+        #choices/ riddles/ tasks
+
+class Game(Frame):
+    def __init__(self, parent):
+        #create the rooms
+
+        #add any items
+
+        #set the current room to r1
+        Game.currentRoom
+
+        #initialize player's inventory
+
+    #set up GUI
+    def setupGUI(self):
+        #organize and pack the GUI
+        self.pack(fill = BOTH, expand = 1)
+        
+        #setup player input(bottom)
+        Game.player_input = Entry(self, bg = "white")
+        Game.player_input.bind("<Return>", self.process)
+        Game.player_input.pack(side=BOTTOM, fill = X)
+        Game.player_input.focus()
+
+        #setup image on the left of the display
+        img = None
+        Game.image = Label(self, width = WIDTH//2, image = img)
+        Game.image.pack(side=LEFT, fill = Y)
+        Game.image.pack_propagate(False) #don't let the img change the widgets size
+
+        #setup text output on the right of the display
+        text_frame = Frame(self, width = WIDTH//2)
+        Game.text = Text(text_frame, bg = "lightgrey", state = DISABLED)
+        Game.text.pack(fill = Y, expand = 1)
+        text_frame.pack(side = RIGHT, fill = Y)
+        text_frame.pack_propagate(False)
+
+        #update current room image
+    def setRoomImage(self):
+        if(Game.currentRoom == None):
+            Game.img = PhotoImage(file="skull.gif")
+        else:
+            Game.img = PhotoImage(file=Game.currentRoom.image)
+
+        Game.image.config(image = Game.img)
+        Game.image.image = Game.img
+
+    #update status
+    def setStatus(self, status):
+        #clear the text widget
+        Game.text.config(state = NORMAL)
+        Game.text.delete("1.0", END)
+
+        #if dead, say so, otherwise set the text to __str__
+        if(Game.currentRoom == None):
+            Game.text.insert(END, "You are dead. You may quit.\m")
+        else:
+            Game.text.insert(END, str(Game.currentRoom)+\
+                             "\nYou are carrying: " +str(Game.inventory) +\
+                             "\n\n" + status)
+            Game.text.config(state = DISABLED)
+            
+    #play the game
+    def play(self):
+        #add rooms
+        self.createRooms()
+        #setup GUI
+        self.setupGUI()
+        #set room image
+        self.setRoomImage()
+        #set status
+        self.setStatus("")
+
+
+#################################################
+#main code( construct the display and begin game)
+#################################################
+
+#define default screen size
+WIDTH = 800
+HEIGHT = 600
+
+#create the window
+window = Tk()
+window.title("Room Adventure")
+
+#create the GUI as a Tkinter canvas within the window
+g = Game(window)
+
+#begin the game
+g.play()
+
+#wait until the main window closes
+window.mainloop()
